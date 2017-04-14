ec2f313e6520b2f2d8ecd5c3cefb9d36f5487ab3
Claire E. Beery <claire.beery@students.olin.edu>
2017-04-14 12:49:50 -0400

    reworked FSM

--- scripts/neato_soccer.py
+++ scripts/neato_soccer.py
@@ -1,41 +1,165 @@
 #!/usr/bin/env python
-import rospy, math, cv2, os, time
+
+""" This is a script that walks through some of the basics of working with images
+    with opencv in ROS. """
+
+import rospy, cv2, math
 import numpy as np
-import automaton as a 
 from cv_bridge import CvBridge
-from geometry_msgs.msg import Point, Twist
-from sensor_msgs.msg import  Image
 from neato_node.msg import Bump
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist, Vector3
 
 
+class NeatoSoccerPlayer(object):
+    """ 
+    The NeatoSoccerPlayer is a Python object that encompasses a ROS node 
+    that can process images from the camera and search for a ball within.
+    The node will issue motor commands to align the center of the camera's
+    field of view with the ball and then move forward  to hit the ball  
+    """
 
-class NeatoSoccerPlayer(a.Automaton):
+    def __init__(self, image_topic):
+        """ Initialize the ball tracker """
 
-    def __init__(self, 
-            name="neato_soccer",
-            states={"DetermineBall":DetermineBall(), "LocateBall":LocateBall(), "Align":Align(), "Kick":Kick()},
-            initialState="DetermineBall",
-            debug=False
-        ):
+        # Setup states
+        #states = determineBall, alignNeato, kickBall]
+        self.state = self.determineBall
+        self.transition = True
 
-        super(NeatoSoccerPlayer, self).__init__(name, states, initialState, debug)
+        # Setup raw camera visualization
+        self.img = None                 #the latest image from the camera
+        self.currentImg = None          #curretn image being processed
+        self.bridge = CvBridge()        #used to convert ROS messages to OpenCV
+        cv2.namedWindow('video_window') #create window for live video stream
 
-        #initialize vision attributes
-        self.img = None         	#raw image from sensor callback
-        self.currentImg = None  	#current image being processed 
-        self.bridge = CvBridge() 
-        cv2.namedWindow('video_window')
+
+        # Setup ROS Node
+        rospy.init_node('neato_soccer')
+        self.rate = rospy.rate(10)
+        rospy.Subscriber(image_topic, Image, self.processImage)
+        rospy.Subscriber("/bump", Bump, self.processBump)
+        self.cmd_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self.cmd = Twist()
 
     def processImage(self, msg):
-        """image callback that stores image"""
-        self.img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="brg8")
+        """
+        Process image messages from ROS and stash them in an attribute
+        called img for subsequent processing
+        """
+        self.img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
 
     def processBump(self, msg):
-        """Signals node shutdown on front bumper trigger"""
-        if msg.leftFront or msg.rightFront:
-	    rospy.signal_shutdown("Kick detected; Node ended")
+        """ End node if ball kicked or neato runs into something """
+        #TODO verfiy "any" call setup
+        if any(bump in msg.values):
+            rospy.signal_shutdown("I kicked a thing!")
 
-if __name__ == "__main__":
-   player = NeatoSoccerPlayer()  
-   player.main()
+    def determineBall(self):
+        """
+        Determines if there is a soccer ball in the view currently being processed
+        using a neural net
+        At Start    > stop neato motion, set currentImg to img
+        Transition 1: condition(ball found) > alignNeato state
+        Transition 2: condition(no ball) > end node
+        """
 
+        # init change state flag
+        ball = None
+
+        # perform state funtionally
+        if transition:
+            "Transistioning to determineBall state"
+            self.cmd = Twist()
+            self.currentImg = self.img
+            self.window_y, self.window_x,_ self.currentImg.shape
+            self.transition = False
+        else:
+            "Looking for ball"
+            ball = neuralnet(self.currentImg)
+
+        # change state if needed
+        if ball:
+            print "Ball exists"
+            self.state = self.alignNeato
+            self.transition = True
+        elif ball is not None:
+            rospy.signal_shutdown("Where ball? Want ball!")     
+
+    def alignNeato(self):
+        """
+        Determines the location of the ball with in the image being processed
+        and rotates neato to line up with ball 
+        At Start    > stop neato motion
+        Transition 1: condition(ball centered) > kickBall state
+        """
+
+        # init change state flag
+        aligned = False
+
+        # perform state funtionally
+        if self.transition:
+            "Transistioning to alignNeato state"
+            self.cmd = Twist()
+            self.transition = False
+
+        else:
+            x,_,_ = colorFilterCOM(self.currentImg)
+            x,_,_ = blobDetector(self.currentImg)
+
+            #determining if ball is within alignment threshold
+            moments = cv2.moments(self.binary_image)
+            if moments['m00'] != 0:
+                x,_ = moments['m10']/moments['m00'], moments['m01']/moments['m00']
+                diff = x - self.window_x
+
+            if math.fabs(diff) < 20:
+                #move forward if within 20 pixels ahead
+                aligned = True
+                break 
+
+            #setting proportional twist control
+            kp = .005           
+            self.cmd.angular.z = -diff*kp
+
+        # change state if needed
+        if aligned:
+            print "Neato aligned with ball"
+            self.state = self.kickBall
+            self.transition = True
+
+    def kickBall(self):
+        """
+        Drive forward to kick ball until node ended
+        At Start    > stop neato motion
+        """
+
+        # perform state funtionally
+        if transition:
+            "Transistioning to kickBall state"
+            self.cmd = Twist()
+        else:
+            self.cmd.linear.x = 0.3 
+
+    def run(self):
+        """ The main run loop"""
+        rospy.on_shutdown(lambda: self.cmd.publish(Twist())
+
+        while not rospy.is_shutdown():
+            if self.img is not None:
+                #show image
+                cv2.imshow('video_window', self.img)
+                cv2.waitKey(5) 
+
+                #perform current state actions
+                self.state()
+            else:
+                self.cmd = Twist()
+
+            #move robot
+            self.cmd_pub.publish(self.cmd)
+            self.rate.sleep()
+
+if __name__ == '__main__':
+    node = NeatoSoccerPlayer("/camera/image_raw")
+    node.run()
