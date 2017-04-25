a41d6c4f270549d69f34d15703571c9b490f1fa0
Claire E. Beery <claire.beery@students.olin.edu>
2017-04-24 20:03:54 -0400

    first pass at hough circles

--- scripts/VisionSuite/accuracyChecker.py
+++ scripts/VisionSuite/accuracyChecker.py
@@ -11,6 +11,7 @@ Visualizes and prints accuracy of a vision suite methods from labeled images
 import os, yaml, math, cv2
 from blobLocator import blobLocator as bl
 from colorFilteredCOM import colorFilteredCOM as cf
+from houghCircles import houghCircles as hc
 
 
 class AccuracyChecker(object):
@@ -54,10 +55,12 @@ class AccuracyChecker(object):
         #Apply visual suite scripts
         bl_center, bl_size = bl(self.img) #simpleBlob detecttor method
         cf_center, cf_size = cf(self.img) #COM after color filter method
+        hc_center, hc_size = hc(self.img) #Hough circles method
 
         #Calculate error
         bl_error = self.calcErr(bl_center, bl_size)
         cf_error = self.calcErr(cf_center, cf_size)
+        hc_error = self.calcErr(hc_center, hc_size)
 
         #Print Error
         print("Blob Method Error: (Blue)" 
@@ -68,24 +71,43 @@ class AccuracyChecker(object):
             + "\nDistance: " + str(cf_error[0]) 
             + "\nSize: " + str(cf_error[1])
             + "\n---")
+        print("Hough Circles Error: (Cyan)" 
+            + "\nDistance: " + str(hc_error[0]) 
+            + "\nSize: " + str(hc_error[1])
+            + "\n---")
 
         #Visualize Error
         self.drawCircle((bl_center),bl_size,(255,0,0))
         self.drawCircle((cf_center),cf_size,(0,255,0))
+        self.drawCircle((hc_center),hc_size,(255,255,0))
 
         cv2.imshow('Visualize Error', self.img)
         cv2.waitKey(0) #close on enter key event
 
 if __name__ == "__main__":
 
+    cur_path = os.path.dirname(os.path.realpath(__file__))  #path to script
+    rel_path = "../../images/"                              #relative path to img folder
+    dir_path = os.path.join(cur_path,rel_path)              #full path to img folder
+    lbl_file = "locator_labels.yaml"                        #label file name
+
+    for i in range(0,7): 
+        img_name = "0" + str(i) + "_neatoSoccer.png" 
+        img_path = os.path.join(dir_path,img_name)              #img path
+        img = cv2.imread(img_path)                              #load from path
+
+        checker = AccuracyChecker(dir_path,img_name,lbl_file)   
+        checker.main()  
+
+    """
     # File Path Components
     cur_path = os.path.dirname(os.path.realpath(__file__)) 	#path to script
     rel_path = "../../images/"								#relative path to img folder
     dir_path = os.path.join(cur_path,rel_path)				#full path to img folder
-    img_name = "00_neatoSoccer.png"									#img name
+    img_name = "05_neatoSoccer.png"									#img name
     lbl_file = "locator_labels.yaml"						#label file name
 
     # Create and Run instance
     checker = AccuracyChecker(dir_path,img_name,lbl_file)	
     checker.main()			
-	
+	"""
